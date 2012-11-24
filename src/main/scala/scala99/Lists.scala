6898e9ed22949a0563b07de76e6ab615e502cb58
Andrew Parker <andrew@avparker.com>
2012-11-24 20:08:33 +0900

    implement P03, P04

--- src/main/scala/scala99/Lists.scala
+++ src/main/scala/scala99/Lists.scala
@@ -2,16 +2,129 @@ package scala99
 
 object Lists {
 
-  def last[A](ls: List[A]): A = ls match {
-    case Nil => throw new NoSuchElementException
-    case x :: Nil => x
-    case x :: xs => last(xs)
+  /*
+   *  P01 (*) Find the last element of a list.
+   *  Example:
+   *  scala> last(List(1, 1, 2, 3, 5, 8))
+   *  res0: Int = 8
+   */
+  def last[A](ls: List[A]): A = {
+    // throw new RuntimeException("Not implemented yet")
+    
+	// implemented without using the built-in method on List
+    ls match {
+      case Nil => throw new NoSuchElementException
+      case x :: Nil => x
+      case x :: xs => last(xs)
+    }
+    
+    // just for fun, an implementation that uses foldLeft
+//    if (ls.isEmpty) throw new NoSuchElementException
+//    else ls.foldLeft(ls.head)( (x, y) => y )
   }
 
-  def penultimate[A](ls: List[A]) : A = ls match {
-    case x :: y :: Nil => x
-    case x :: xs => penultimate(xs)
-    case _ => throw new NoSuchElementException
+  /*
+   * P02 (*) Find the last but one element of a list.
+   * Example:
+   * scala> penultimate(List(1, 1, 2, 3, 5, 8))
+   * res0: Int = 5
+   */
+  def penultimate[A](ls: List[A]): A = {
+    // throw new RuntimeException("Not implemented yet")
+    ls match {
+      case x :: y :: Nil => x
+      case x :: xs => penultimate(xs) // this list has 1 or >2 entries
+      case _ => throw new NoSuchElementException
+    }
+  }
+
+  /*
+   * P03 (*) Find the Nth element of a list.
+   * By convention, the first element in the list is element 0.
+   * Example:
+   * scala> nth(2, List(1, 1, 2, 3, 5, 8))
+   * res0: Int = 2
+   */
+  def nth[A](n: Int, ls: List[A]): A = {
+    // throw new RuntimeException("Not implemented yet")
+    ls match {
+      case Nil => throw new NoSuchElementException
+      case x :: xs =>
+        if (n == 0) x
+        else nth(n - 1, xs)
+    }
+  }
+
+  /*
+   * P04 (*) Find the number of elements of a list.
+   * Example:
+   * scala> length(List(1, 1, 2, 3, 5, 8))
+   * res0: Int = 6
+   */
+  def length[A](ls: List[A]): Int = {
+    //throw new RuntimeException("Not implemented yet")
+    
+//	// implement with a tail-recursive helper function
+//    def length0(n: Int, ls: List[A]): Int = {
+//      ls match {
+//        case Nil => n
+//        case _ => length0(n+1, ls.tail)
+//      }
+//    }
+//    length0(0, ls)
+    
+    // start with 0, add one for each element in the list
+    ls.foldLeft(0)( (x, _) => x + 1 )
+  }
+
+  /*
+   * P05 (*) Reverse a list.
+   * Example:
+   * scala> reverse(List(1, 1, 2, 3, 5, 8))
+   * res0: List[Int] = List(8, 5, 3, 2, 1, 1)
+   */
+  def reverse[A](ls: List[A]): List[A] = {
+    throw new RuntimeException("Not implemented yet")
+  }
+
+  def flatten(ls: List[Any]): List[Any] = {
+    throw new RuntimeException("Not implemented yet")
+  }
+
+  def compress[A](ls: List[A]): List[A] = {
+    throw new RuntimeException("Not implemented yet")
+  }
+
+  def pack[A](ls: List[A]): List[List[A]] = {
+    throw new RuntimeException("Not implemented yet")
+  }
+
+  def encode[A](ls: List[A]): List[(Int, A)] = {
+    throw new RuntimeException("Not implemented yet")
+  }
+
+  def encodeModified[A](ls: List[A]): List[Any] = {
+    throw new RuntimeException("Not implemented yet")
+  }
+
+  def decode[A](ls: List[(Int, A)]): List[A] = {
+    throw new RuntimeException("Not implemented yet")
+  }
+
+  def encodeDirect[A](ls: List[A]): List[Any] = {
+    throw new RuntimeException("Not implemented yet")
+  }
+
+  def duplicate[A](ls: List[A]): List[A] = {
+    throw new RuntimeException("Not implemented yet")
+  }
+
+  def duplicateN[A](n: Int, ls: List[A]): List[A] = {
+    throw new RuntimeException("Not implemented yet")
+  }
+
+  def drop[A](n: Int, ls: List[A]): List[A] = {
+    throw new RuntimeException("Not implemented yet")
   }
 
 }
--- src/test/scala/scala99/ListsSuite.scala
+++ src/test/scala/scala99/ListsSuite.scala
@@ -5,6 +5,8 @@ import org.scalatest.FunSuite
 import org.junit.runner.RunWith
 import org.scalatest.junit.JUnitRunner
 
+// Unit tests liberally copied from https://github.com/danluu/ninety-nine-scala-problems
+// Added some tests cases for handling exceptional cases
 @RunWith(classOf[JUnitRunner])
 class ListsSuite extends FunSuite {
   import scala99.Lists._
@@ -20,6 +22,22 @@ class ListsSuite extends FunSuite {
     assert(penultimate(List(1, 2)) === 1)
     assert(penultimate(List(1, 2, 3)) === 2)
     assert(penultimate(List(1, 1, 2, 3, 5, 8)) === 5)
-    intercept[NoSuchElementException] { penultimate(List('x')) }
+    intercept[NoSuchElementException] { penultimate(List()) }
+    intercept[NoSuchElementException] { penultimate(List(1)) }
+  }
+
+  test("P03 -- nth element of list") {
+    assert(nth(0, List(1, 1, 2, 3, 5, 8)) === 1)
+    assert(nth(1, List(1, 1, 2, 3, 5, 8)) === 1)
+    assert(nth(2, List(1, 1, 2, 3, 5, 8)) === 2)
+    assert(nth(5, List(1, 1, 2, 3, 5, 8)) === 8)
+    intercept[NoSuchElementException] { nth(42, List(1, 1, 2, 3, 5, 8)) }
+    intercept[NoSuchElementException] { nth(0, List()) }
+  }
+
+  test("P04 -- length of a list") {
+    assert(length(List()) === 0)
+    assert(length(List(1)) === 1)
+    assert(length(List(1, 1, 2, 3, 5, 8)) === 6)
   }
 }
